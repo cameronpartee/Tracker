bbd82852f87c9c3230825e3e8fc9061b17c019d7
cameronpartee <cameronepartee@gmail.com>
2022-01-27 22:54:20 -0500

    move logic into components

--- src/App.js
+++ src/App.js
@@ -1,74 +1,22 @@
 import * as React from "react";
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import TableHead from "@mui/material/TableHead";
-import TableBody from "@mui/material/TableBody";
 import { buckets } from "./data/data";
-import { useState, useEffect } from "react";
-import DropdownControl from "./components/DropdownControl";
-import { Button } from "@mui/material";
+import { useState } from "react";
+import InterviewQuestionForm from "./components/InterviewQuestionForm";
+import DataProgressionTable from "./components/DataProgressionTable";
+import IntroGreeting from "./components/IntroGreeting";
 
 function App() {
   const [tableData, setTableData] = useState(buckets);
-  const [newData, setNewData] = useState({});
-  const [newTableData, setNewTableData] = useState([]);
-
-  const hour = new Date().getHours();
-  const timeOfDay =
-    (hour < 12 && "morning") || (hour < 17 && "afternoon") || "evening";
-
-  useEffect(() => {
-    if (newTableData.some((data) => data.name === newData.name)) {
-      setNewTableData((prevTableData) =>
-        prevTableData.filter((data) => data.name !== newData.name)
-      );
-    }
-    setNewTableData((prevTableData) => [...prevTableData, newData]);
-  }, [newData]);
-
-  useEffect(() => {
-    console.log(newTableData);
-  }, [newTableData]);
 
   return (
     <div>
-      <div>
-        <h1>{`Good ${timeOfDay} Cameron`}</h1>
-        <div>How many interview questions have you completed today?</div>
-        {/* Lets add a positive word api here lol */}
-        <DropdownControl name={tableData[0].name} newTableData={setNewData} />
-        <DropdownControl name={tableData[1].name} newTableData={setNewData} />
-        <DropdownControl name={tableData[2].name} newTableData={setNewData} />
-
-        <Button>Click me</Button>
-        <TableContainer
-          style={{ width: "25%" }}
-          component={Paper}
-          variant="outlined"
-        >
-          <Table aria-label="demo table">
-            <TableHead>
-              <TableRow>
-                <TableCell>{tableData[0].name}</TableCell>
-                <TableCell>{`${tableData[0].completed}/${tableData[0].total}`}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>{tableData[1].name}</TableCell>
-                <TableCell>{`${tableData[1].completed}/${tableData[1].total}`}</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>{tableData[2].name}</TableCell>
-                <TableCell>{`${tableData[2].completed}/${tableData[2].total}`}</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+      <IntroGreeting />
+      <br />
+      <InterviewQuestionForm
+        tableData={tableData}
+        setTableData={setTableData}
+      />
+      <DataProgressionTable tableData={tableData} />
     </div>
   );
 }

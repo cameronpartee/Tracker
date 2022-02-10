9e090a97ce5787cc07685c8fba89b69e7bea8ea5
cameronpartee <cameronepartee@gmail.com>
2022-02-10 13:47:07 -0500

    add a material ui switch control

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import IntroGreeting from "./components/IntroGreeting";
 import Chart from "./components/Chart";
 import style from "./style/app.module.css";
 import { useState } from "react";
+import { weekly, annual } from "./helpers/constants";
+import {
+  getWeeklyChartTimeWindow,
+  getAnnualChartTimeWindow,
+} from "./helpers/helper";
+import ChartSwitch from "./components/ChartSwitch";
 
 function App() {
   const [axis, setAxis] = useState("x");
+  const [chartSwitch, setChartSwitch] = useState(true);
 
   const switchAxis = () => {
     setAxis((prevAxis) => (prevAxis === "x" ? "y" : "x"));
@@ -14,8 +21,26 @@ function App() {
   return (
     <div className={style.Container}>
       <IntroGreeting />
-      <Chart axis={axis} />
-      <button onClick={switchAxis}>Switch chart axis</button>
+      <div className={style.WeeklyChart}>
+        <Chart
+          axis={axis}
+          chartType={weekly}
+          chartSwitch={chartSwitch}
+          timeWindow={getWeeklyChartTimeWindow()}
+        />
+      </div>
+      <div className={style.ControlsPannel}>
+        <button onClick={switchAxis}>Switch chart axis</button>
+        <ChartSwitch setChartSwitch={setChartSwitch} />
+      </div>
+      <div className={style.AnnualChart}>
+        <Chart
+          axis={"x"}
+          chartType={annual}
+          chartSwitch={chartSwitch}
+          timeWindow={getAnnualChartTimeWindow()}
+        />
+      </div>
     </div>
   );
 }

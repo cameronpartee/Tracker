5c591fd9af73676a155f4abcbf5aa100cd742af3
cameronpartee <cameronepartee@gmail.com>
2022-02-22 23:26:10 -0500

    added useMemo hook

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import IntroGreeting from "./components/IntroGreeting";
 import Chart from "./components/Chart";
 import style from "./style/app.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { weekly, annual } from "./helpers/constants";
 import {
   getWeeklyChartTimeWindow,
@@ -14,9 +14,12 @@ function App() {
   const [axis, setAxis] = useState("x");
   const [chartSwitch, setChartSwitch] = useState(true);
 
-  const switchAxis = () => {
-    setAxis((prevAxis) => (prevAxis === "x" ? "y" : "x"));
-  };
+  const switchAxis = React.useMemo(
+    () => () => {
+      setAxis((prevAxis) => (prevAxis === "x" ? "y" : "x"));
+    },
+    [axis]
+  );
 
   return (
     <div className={style.Container}>
@@ -33,11 +36,12 @@ function App() {
         <button onClick={switchAxis}>Switch chart axis</button>
         <ChartSwitch setChartSwitch={setChartSwitch} />
       </div>
+      {/* <AddChartLabel /> */}
       <div className={style.AnnualChart}>
         <Chart
           axis={"x"}
           chartType={annual}
-          chartSwitch={chartSwitch}
+          chartSwitch={null}
           timeWindow={getAnnualChartTimeWindow()}
         />
       </div>

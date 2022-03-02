fd65fdb8f393738e215465cb1884ee6de4f24462
cameronpartee <cameronepartee@gmail.com>
2022-03-02 08:41:27 -0500

    use styled components for modal

---
+++ src/components/Layout/Modal.js
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "../../style/modal.module.css";
+
+export default class Modal extends React.Component {
+  render() {
+    if (!this.props.show) {
+      return null;
+    }
+    return (
+      <div className={style.Modal} id="modal">
+        <h2 className={style.ModalTitle}>Add category</h2>
+        <div className={style.Content}>{this.props.children}</div>
+        <div className={style.Actions}>
+          <button
+            className={style.ToggleButton}
+            onClick={(e) => {
+              this.props.hide(false);
+            }}
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
